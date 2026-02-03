fd4ec39b69f6f4fadffe02d2cdad7598cbae5bd3
Rino Godson <rinogodson@gmail.com>
2026-02-03 20:20:00 +0530

    NEW CONFS

--- sketchybar/items/calendar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item calendar q \
-           --set calendar \
-                          update_freq=1 \
-                          script="$PLUGIN_DIR/calendar.sh"
--- sketchybar/items/time.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item time e \
-           --set time \
-                          update_freq=1 \
-                          script="$PLUGIN_DIR/time.sh"
